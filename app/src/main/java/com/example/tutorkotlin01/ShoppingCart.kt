7c7b938fd875c7dc57a99e11df7971c1bf9ef8ee
skripsi <frenone10@gmail.com>
2022-09-07 21:57:07 +0700

    tambah shopping cart

---
+++ app/src/main/java/com/example/tutorkotlin01/ShoppingCart.kt
@@ -0,0 +1,11 @@
+package com.example.tutorkotlin01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShoppingCart : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_cart)
+    }
+}
